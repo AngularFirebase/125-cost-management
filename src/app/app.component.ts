aefc46e147a8214ba0e49048310ba3435dea58d8
codediodeio <hello@codediode.io>
2018-08-04 12:04:22 -0700

    full working demp fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map, tap, shareReplay } from 'rxjs/operators';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agg';
+  donations: Observable<any[]>;
+  total;
+  average;
+
+  data$;
+  constructor(private afs: AngularFirestore) {
+    this.data$ = afs.doc('aggregation/donations').valueChanges();
+
+    // this.donations = afs
+    //   .collection('donations')
+    //   .valueChanges()
+    //   .pipe(
+    //     tap(arr => console.log(`read ${arr.length} docs`)),
+    //     shareReplay(1)
+    //   );
+
+    // this.total = this.donations.pipe(
+    //   map(arr => arr.reduce((total, current) => total + current.amount, 0))
+    // );
+
+    // this.average = this.donations.pipe(
+    //   map(
+    //     arr =>
+    //       arr.reduce((total, current) => total + current.amount, 0) / arr.length
+    //   )
+    // );
+  }
+
+  seed() {
+    this.afs.collection('donations').add({
+      username: faker.name.findName(),
+      amount: parseFloat(faker.finance.amount(5, 500))
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+const config = {
+  apiKey: 'AIzaSyBogpc-AUVgVdcgoiWhE4_lKCFzfcbaSaA',
+  authDomain: 'angularfirebase-267db.firebaseapp.com',
+  databaseURL: 'https://angularfirebase-267db.firebaseio.com',
+  projectId: 'angularfirebase-267db',
+  storageBucket: 'angularfirebase-267db.appspot.com',
+  messagingSenderId: '96751198234'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
